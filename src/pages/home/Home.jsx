05b5d8a1c474e94bb4eb6d0d5f4c132848827a4b
Halina Tulchii <halendeihalyna@gmail.com>
2024-12-02 00:27:51 +0200

    added images

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,51 @@
 import React from "react";
 import "./Home.css";
+import Brightspace from '../../img/home/Brightspace.png';
+import Brightspace1 from '../../img/home/Brightspace1.png';
+import Brightspace2 from '../../img/home/Brightspace2.png';
 
 const Home = () => {
+
+  // useEffect(() => {
+  //   const body = document.body;
+  //   if (activeItem) {
+  //     body.classList.add("red");
+  //   } else {
+  //     body.classList.remove("red");
+  //   }
+  // }, [activeItem]);
+
   return (
     <section className="home__container container">
       <div className="corner-top-left"></div>
       <div className="corner-top-right"></div>
       <div className="corner-bottom-left"></div>
       <div className="corner-bottom-right"></div>
-      <div className="home-background"></div>
+      <div className="home-background">
+        <ul className="upline">
+          <li className="upline__item"></li>
+          <li className="upline__item upline__item--middle"></li>
+          <li className="upline__item"></li>
+        </ul>
+
+
+        <ul className="middleline">
+          <li className="middleline__item random-img">
+            <img src={Brightspace1} alt="img" className="middleline__img"/>
+          </li>
+          <li className="middleline__item middleline__item--middle">
+            <img src={Brightspace} alt="" className="middleline__img middleline__img--middle"/>
+          </li>
+          <li className="middleline__item random-img">
+            <img src={Brightspace2} alt="" className="middleline__img"/>
+          </li>
+        </ul>
+        <ul className="downline">
+          <li className="downline__item"></li>
+          <li className="downline__item downline__item--middle"></li>
+          <li className="downline__item"></li>
+        </ul>
+      </div>
 
       <div className="home__content">
         <p className="home__text">
@@ -19,17 +56,21 @@ const Home = () => {
         <div className="home__downpart">
           <div className="home__listsWrapper">
             <ul className="home__list">
-              <li className="home__item home__item--uppercase">Bright space</li>
-              <li className="home__item">estate project</li>
-              <li className="home__item home__item--uppercase">ArchShots</li>
-              <li className="home__item">architecture</li>
+              <li className="home__item "> 
+                <span className="home__item--uppercase">Bright space</span>
+              estate project</li>
+              <li className="home__item "> 
+                <span className="home__item--uppercase">ArchShots</span> 
+              architecture</li>
             </ul>
 
             <ul className="home__list home__list--right">
-              <li className="home__item home__item--uppercase">DroneVista</li>
-              <li className="home__item">Drone Photography</li>
-              <li className="home__item home__item--uppercase">SpaceLens</li>
-              <li className="home__item">Commercial Spaces</li>
+            <li className="home__item "> 
+              <span className="home__item--uppercase">DroneVista</span> 
+            drone Photography</li>
+            <li className="home__item "> 
+              <span className="home__item--uppercase">SpaceLens</span>
+            commercial Spaces</li>
             </ul>
           </div>
 
